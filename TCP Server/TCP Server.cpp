b01b6dc795afa1c6bcf9896f40229be800b17360
Synthesized Artistry <SynthesizedArtistry@gmail.com>
2024-02-16 13:38:01 -0500

    Start over and simply read Html
    Trying to work with simple Cpp code is better than the complicated Winsock2 guide on Microsoft. Rewrite and keep it simple.

--- TCP Server/TCP Server.cpp
+++ TCP Server/TCP Server.cpp
@@ -8,48 +8,93 @@
 
 int main()
 {
+    /*** WINDOWS SOCKET WSA INIT NEEDED ***/
+    WORD wVersionRequested;
+    WSADATA wsaData; 
+    int err;
+    
+    wVersionRequested = MAKEWORD(2, 2);
+    err = WSAStartup(wVersionRequested, &wsaData);
+    if (err != 0) {
+        printf("WSAStartup failed with error: %d\n", err);
+        return 1;
+    }
+    /*** END ***/
+
+    FILE* html_data;
+    errno_t errn = fopen_s(&html_data, "C:\\Users\\alla0\\Documents\\GitHub\\TCP-Server\\TCP Server\\index.html", "r");
+    if (html_data == NULL) {
+        printf("Cannot open file no: %d\n", errn);
+    }
+    char buffer[128] = {0};
+    char response_data[2048] = {0};
+
+    while (fgets(buffer, sizeof(buffer), html_data)) {
+        strcat_s(response_data, buffer);
+    }
+    char http_header[101] = { 0 }; // modern browsers dont handle headers well! must create a function that sets the size of this to the file size;
+    strcat_s(http_header, "HTTP/1.1 200 OK\r\n\n"); // add this to the index.html then calculate size of html file
+    strcat_s(http_header, response_data);
+    
+    // create socket
     int socketfd = socket(AF_INET, SOCK_STREAM, 0);
 
-    struct sockaddr_in address = {
-        AF_INET,
-        htons(9999),
-        0
-    };
+    struct sockaddr_in address = { 0 };
+    address.sin_family = AF_INET;
+    address.sin_port = htons(6969);
+    address.sin_addr.s_addr = INADDR_ANY;
 
-    if (bind(socketfd, (struct sockaddr*)&address, sizeof(address)) < 0) {
-        printf("NOT BINDED\n");
+    if (bind(socketfd, (struct sockaddr*)&address, sizeof(address))) {
+        printf("NOT BINDED: %d\n", WSAGetLastError());
+        return 1;
     }
+    printf("Bind to port 6969\n");
 
-    listen(socketfd, 10);
+    if (listen(socketfd, 5)) {
+        printf("NOT LISTENING: %d\n", WSAGetLastError());
+        return 1;
+    }
+    printf("Listening to port 6969\n");
+    int clientfd;
+    while (1) {
+        char client_msg[4096] = "";
+        
+        clientfd = accept(socketfd, NULL, NULL);
+        
+        /*
+        _read(clientfd, client_msg, 4095);
+        printf("%s\n", client_msg);
 
-    int clientfd = accept(socketfd, 0, 0);
-    // stdin - 0
-    struct pollfd fds[2] = {
-        {    
-            0,
-            POLLIN,
-            0
-        },
-        {
-            clientfd,
-            POLLIN,
-            0
-        }
-    };
-    for (;;) {
-        char buffer[256] = { 0 };
-        WSAPoll(fds, 2, 50000);
-        if (fds[0].revents & POLLIN) {
-            _read(0, buffer, 255);
-            send(clientfd, buffer, 255, 0);
-        }
-        else if (fds[1].revents & POLLIN) {
-            if (recv(clientfd, buffer, 255, 0) == 0) {
-                return 0;
+        
+        char* method = NULL;
+        char* route = NULL;
+        char* client_http_header;
+        strtok_s(client_msg, "\n", &client_http_header);
+        printf("%s\n\n", client_http_header);
+
+        char* header_token;
+        strtok_s(client_http_header, " ", &header_token);
+
+        int header_parse_counter = 0;
+
+        while (header_token != NULL) {
+            switch (header_parse_counter) {
+            case 0:
+                method = header_token;
+            case 1:
+                route = header_token;
             }
-            printf("%s\n",buffer);
         }
+
+        printf("The method is: %s\n", method);
+        printf("The route is: %s\n", route);
+        */
+        // check if file descriptor is valid; if not then stop trying to use it
+        
+        send(clientfd, http_header, sizeof(http_header), 0);
+        //_close(clientfd);
     }
+    fclose(html_data);
 
     return 0;
 }
